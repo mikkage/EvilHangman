170298d73bd3ac7b1e2c13bf14bcec36a8ceecaa
Michael Parker <msparker@ucsc.edu>
2014-02-10 11:05:30 -0800

    added test for min and max length of words in dictionary

--- Resources/iotest.hs
+++ Resources/iotest.hs
@@ -8,6 +8,12 @@ main = do
 	putStrLn "Enter the length of words to filter:"
 	len <-getLine
 	let l = read len :: Int
+	let o = "Words of length " ++ len ++ " filtered out from dictionary.txt to out.txt"
+	putStrLn o
+	let o1 = "Shortest word length: " ++ show (minimum (map length s))
+	putStrLn o1
+	let o2 = "Longest word length: " ++ show (maximum (map length s))
+	putStrLn o2
 	writeFile "out.txt" (filterDictionary l s)
 
 filterDictionary :: Int -> [String] -> String

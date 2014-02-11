3c06c6f52d51756dee74b49ae7186244aac7c950
Michael Parker <msparker@ucsc.edu>
2014-02-10 17:01:53 -0800

    added character filtering

--- Resources/iotest.hs
+++ Resources/iotest.hs
@@ -5,16 +5,24 @@ main ::IO ()
 main = do
 	inpStr <- readFile "dictionary.txt"
 	let s = words inpStr
+
 	putStrLn "Enter the length of words to filter:"
 	len <-getLine
 	let l = read len :: Int
+
+	putStrLn "Enter a character to filter out:"
+	letter <- getChar
+	
 	let o = "Words of length " ++ len ++ " filtered out from dictionary.txt to out.txt"
 	putStrLn o
 	let o1 = "Shortest word length: " ++ show (minimum (map length s))
 	putStrLn o1
 	let o2 = "Longest word length: " ++ show (maximum (map length s))
 	putStrLn o2
-	writeFile "out.txt" (filterDictionary l s)
+	
+	let out = filterDictionary l s
+	writeFile "out.txt" out
+	writeFile "outfiltered.txt" (filterLetter letter (lines out))
 
 filterDictionary :: Int -> [String] -> String
 filterDictionary len (x:xs)
@@ -24,3 +32,12 @@ filterDictionary len [x]
 	| len == (length x) = x
 	| otherwise = ""
 filterDictionary _ [] = ""
+
+filterLetter :: Char -> [String] -> String
+filterLetter letter (x:xs)
+	| (elem letter x) == False = x ++ "\n" ++ filterLetter letter xs
+	| otherwise = "" ++ filterLetter letter xs
+filterLetter letter [x]
+	| (elem letter x) == False = x
+	| otherwise = ""
+filterLetter _ [] = ""

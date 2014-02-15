b7baf056ac6dcb91f89ff76b58eaea7b9565c3f1
Reid A <ranetsbe@ucsc.edu>
2014-02-14 17:59:16 -0800

    filterPos

--- Haskell/iotest.hs
+++ Haskell/iotest.hs
@@ -1,41 +1,5 @@
 import System.IO
 
-main :: IO ()
-main = do
-	inpStr <- readFile "dictionary.txt"
-	let s = words inpStr
-
-	-- insane hangman graphics
-	-- 6 files (0 - 5)
-	-- hangman0 is start, hangman5 is end
-	-- could add more if 5 guesses isnt enough
-	inpStr <- readFile "hangman5.txt"
-	let hangman = inpStr
-	putStrLn hangman
-
-	putStrLn "Enter the length of the word:"
-	len <-getLine
-	let l = read len :: Int
-
-	let d = filterDictionary l s
-
-	playGame d [] 30	--start with 30 guesses, just for testing
-
-	putStrLn ""
-
-	--putStrLn "Enter a character to filter out:"
-	--letter <- getChar
-	
-	--let o = "Words of length " ++ len ++ " filtered out from dictionary.txt to --out.txt"
-	--putStrLn o
-	--let o1 = "Shortest word length: " ++ show (minimum (map length s))
-	--putStrLn o1
-	--let o2 = "Longest word length: " ++ show (maximum (map length s))
-	--putStrLn o2
-	
-	--let out = (filterDictionary l s)
-	--writeFile "out.txt" (unwords out)
-	--writeFile "outfiltered.txt" (unwords (filterLetter letter out))
 
 --------------------------------------------------
 ----------------Filter Dictionary-----------------
@@ -50,7 +14,7 @@ filterDictionary len str = filter (checkStrLen len) str
 --   returns True if the length of the string is equal to the integer,
 --   else False.
 checkStrLen :: Int -> String -> Bool
-checkStrLen len str 
+checkStrLen len str
 	| len == length str = True
 	| otherwise         = False
 
@@ -72,6 +36,15 @@ elemOfAll c (x:xs)
 --------------------------------------------------
 filterLetter :: Char -> [String] -> [String]
 filterLetter c str = filter (c `notElem`) str
+
+filterPos :: String -> [String] -> [String]
+filterPos s strs = filter (compareStr s) strs
+
+compareStr :: String -> String -> Bool
+compareStr [] [] = True
+compareStr (a:as) (b:bs)
+	| a == '-' || a == b = compareStr as bs
+	| otherwise          = False
 
 --------------------------------------------------
 -------------------Display Word-------------------
@@ -114,8 +87,7 @@ playGame dic guesses remain = do
 			putStrLn dis
 			putStrLn "You win this time..."
 		else do
-			putStr ""
-			putStrLn ("Letters guessed so far: " ++ guesses)	--show letters guesses so far
+			putStrLn ("Letters guessed so far: " ++ guesses) --show letters guesses so far
 			putStrLn (show remain ++ " guesses remain")	--show remaining guesses
 			putStrLn (head dic)				--print uncovered word(for testing)
 			putStrLn dis
@@ -123,9 +95,8 @@ playGame dic guesses remain = do
 			guess <- getCharInput guesses
 			let a = filterLetter guess dic
 			if a == []
-			then playGame dic (guesses ++ [guess]) (remain -1)
-			else playGame a (guesses ++ [guess]) (remain -1)
-			putStr ""
+			then playGame dic (guesses ++ [guess]) (remain - 1)
+			else playGame a (guesses ++ [guess]) (remain - 1)
 
 -- qsort
 --   quicksort in 3 lines (not in place)

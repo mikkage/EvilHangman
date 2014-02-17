b5b629f37efb16c111d10307e37ca9a4aacf5440
Michael Parker <msparker@ucsc.edu>
2014-02-16 21:53:45 -0800

    cleaned up and added the remaining function descriptions

--- Haskell/evilHangman.hs
+++ Haskell/evilHangman.hs
@@ -15,7 +15,6 @@ main = do
 --Takes in an Int and list of Strings, and returns
 --only the strings of that Int's length, separated
 --by new lines in a single string.
---------------------------------------------------
 filterDictionary :: Int -> [String] -> [String]
 filterDictionary len str = filter (checkStrLen len) str
 
@@ -41,14 +40,22 @@ elemOfAll c (x:xs)
 --Takes in a character and a list of strings, and
 --returns a new list of strings that do not
 --contain that letter.
---------------------------------------------------
 filterLetter :: Char -> [String] -> [String]
 filterLetter c str = filter (c `notElem`) str
 
+-------------------Filter Position----------------
+--Takes a string of characters that have been guessed by the user,
+--the dictionary list, and the partially revealed word, then returns a new
+--list of strings which contains words that match the pattern of
+--the parts of the word which has been revealed.
+--EX: any words that might fit some pattern like: ---e--a--b
 filterPos :: String -> [String] -> String -> [String]
 filterPos s strs g = filter (compareStr g s) strs
 
-
+----------------Compare String--------------------
+--Takes in the fully revealed word, the partially revealed word,
+--and the list of which letters have been guessed, and returns
+--true if the strings match, false if not.
 compareStr :: String -> String -> String -> Bool
 compareStr _ [] [] = True
 compareStr g (a:as) (b:bs)
@@ -65,7 +72,6 @@ compareStr [] (a:as) (b:bs)
 --have been guessed by the user, and display either
 --'-' for letters that have not been guessed or the
 --letters of those which have been guessed.
---------------------------------------------------
 displayWord :: String -> String -> String
 displayWord (w:ws) guesses
 	| (elem w guesses) == True = [w] ++ displayWord ws guesses
